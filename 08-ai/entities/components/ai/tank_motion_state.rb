3e80a6e7ccf2823fbccf2d4212b40b5ef7244cae
stain88 <marcbaghdadi@gmail.com>
2017-10-30 17:26:52 +0000

    Updated AI Tank Motion State

--- 08-ai/entities/components/ai/tank_motion_state.rb
+++ 08-ai/entities/components/ai/tank_motion_state.rb
@@ -1,8 +1,15 @@
 class TankMotionState
+  STATE_CHANGE_DELAY = 500
 
-  def initialize(object, vision)
+  def initialize(object, vision, gun)
     @object = object
     @vision = vision
+    @gun = gun
+    @roaming_state = TankRoamingState.new(object, vision)
+    @fighting_state = TankFightingState.new(object, vision)
+    @fleeing_state = TankFleeingState.new(object, vision, gun)
+    @chasing_state = TankChasingState.new(object, vision, gun)
+    set_state(@roaming_state)
   end
 
   def enter
@@ -26,7 +33,38 @@ class TankMotionState
   end
 
   def update
-    # Override
+    choose_state
+    @current_state.update
+  end
+
+  def set_state(state)
+    return unless state
+    return if state == @current_state
+    @last_state_change = Gosu.milliseconds
+    @current_state = state
+    state.enter
+  end
+
+  def choose_state
+    return unless Gosu.milliseconds - (@last_state_change) > STATE_CHANGE_DELAY
+    if @gun.target
+      if @object.health.health > 40
+        if @gun.distance_to_target > BulletPhysics::MAX_DIST
+          new_state = @chasing_state
+        else
+          new_state = @fighting_state
+        end
+      else
+        if @fleeing_state.can_flee?
+          new_state = @fleeing_state
+        else
+          new_state = @fighting_state
+        end
+      end
+    else
+      new_state = @roaming_state
+    end
+    set_state(new_state)
   end
 
   def wait
@@ -61,19 +99,13 @@ class TankMotionState
   end
 
   def on_collision(with)
-    change = case rand(0..100)
-    when 0..30
-      -90
-    when 30..60
-      90
-    when 60..70
-      135
-    when 80..90
-      -135
-    else
-      180
+    @current_state.on_collision(with)
+  end
+
+  def on_damage(amount)
+    if @current_state == @roaming_state
+      set_state(@fighting_state)
     end
-    @object.physics.change_direction(@object.direction + change)
   end
 
 end
