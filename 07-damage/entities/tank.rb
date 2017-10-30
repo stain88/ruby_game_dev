2d96dfb5e5912bd97bd26952598fe06b6dba7362
stain88 <marcbaghdadi@gmail.com>
2017-10-30 14:46:25 +0000

    Adding Health component

--- 07-damage/entities/tank.rb
+++ 07-damage/entities/tank.rb
@@ -1,6 +1,6 @@
 class Tank < GameObject
   SHOOT_DELAY = 500
-  attr_accessor :x, :y, :throttle_down, :direction, :gun_angle, :sounds, :physics, :graphics
+  attr_accessor :x, :y, :throttle_down, :direction, :gun_angle, :sounds, :physics, :graphics, :health
 
   def initialize(object_pool, input)
     super(object_pool)
@@ -11,6 +11,7 @@ class Tank < GameObject
     @sounds = TankSounds.new(self)
     @direction = rand(0..7) * 45
     @gun_angle = rand(0..360)
+    @health = TankHealth.new(self, object_pool)
   end
 
   def shoot(target_x, target_y)
